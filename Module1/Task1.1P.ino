24b0f6864e8f0518de80da4a6735b973f097822b
Jet <67730137+jetapps1@users.noreply.github.com>
2022-07-02 16:32:23 +1000

    Add code to Task1.1P

--- Module1/Task1.1P.ino
+++ Module1/Task1.1P.ino
@@ -1,1 +1,23 @@
+int MOTION_SENSOR_PIN = 2; // Arduino pin connected to the OUTPUT pin of motion sensor
+int LED_PIN = 8; // Arduino pin connected to the input pin of the LED
 
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT); // Set Arduino pin to output mode
+  pinMode(MOTION_SENSOR_PIN, INPUT); // Set Arduino pin to input mode
+  Serial.begin(9600); // Initialize Serial
+}
+
+void loop()
+{
+  int currentMotion = digitalRead(MOTION_SENSOR_PIN); // Store the Motion Sensor's Current State
+  
+  if(currentMotion==HIGH) { // If motion is detected
+    Serial.println("Motion detected"); // Print "Motion Detected" to serial
+  	digitalWrite(LED_PIN, HIGH); // Turn LED on
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+  else {
+    digitalWrite(LED_PIN, LOW); // Turn LED off
+  }
+}
